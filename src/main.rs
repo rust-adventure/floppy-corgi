fa90f1b97653e664e34645ce1a27b38951e8660d
Christopher Biscardi <chris@christopherbiscardi.com>
2025-07-31 21:39:01 -0700

    003-camera2d-projection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera};
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, startup)
+        .run()
+}
+
+fn startup(mut commands: Commands) {
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scaling_mode: camera::ScalingMode::AutoMax {
+                max_width: 480.,
+                max_height: 270.,
+            },
+            ..OrthographicProjection::default_2d()
+        }),
+    ));
+
+    commands.spawn((
+        Sprite {
+            custom_size: Some(Vec2::splat(25.)),
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 1.0),
+    ));
 }
