cf5e4499f1aa9037e983d1f7dd555236073738dc
Christopher Biscardi <chris@christopherbiscardi.com>
2026-07-05 10:44:41 -0700

    bsn!

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use std::time::Duration;
 
-use bevy::{
-    image::ImageLoaderSettings, prelude::*,
-    time::common_conditions::on_timer,
-};
+use bevy::{image::ImageLoaderSettings, prelude::*, time::common_conditions::on_timer};
 
 pub const CANVAS_SIZE: Vec2 = Vec2::new(480., 270.);
 pub const PLAYER_SIZE: f32 = 25.0;
@@ -20,129 +17,98 @@ impl Plugin for PipePlugin {
             (
                 despawn_pipes,
                 shift_pipes_to_the_left,
-                spawn_pipes.run_if(on_timer(
-                    Duration::from_millis(1000),
-                )),
+                spawn_pipes.run_if(on_timer(Duration::from_millis(1000))),
             ),
         );
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 pub struct Pipe;
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 pub struct PipeTop;
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 pub struct PipeBottom;
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 pub struct PointsGate;
 
-fn spawn_pipes(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    time: Res<Time>,
-) {
-    let image = asset_server.load_with_settings(
-        "pipe.png",
-        |settings: &mut ImageLoaderSettings| {
+fn spawn_pipes(mut commands: Commands, asset_server: Res<AssetServer>, time: Res<Time>) {
+    let image =
+        asset_server.load_with_settings("pipe.png", |settings: &mut ImageLoaderSettings| {
             settings
                 .sampler
                 .get_or_init_descriptor()
-                .set_filter(
-                    bevy::image::ImageFilterMode::Nearest,
-                );
-        },
-    );
-
-    let image_mode =
-        SpriteImageMode::Sliced(TextureSlicer {
-            border: BorderRect::axes(8., 19.),
-            center_scale_mode: SliceScaleMode::Stretch,
-            ..default()
+                .set_filter(bevy::image::ImageFilterMode::Nearest);
         });
 
-    let transform =
-        Transform::from_xyz(CANVAS_SIZE.x / 2., 0.0, 1.0);
-    let gap_y_position = (time.elapsed_secs() * 4.2309875)
-        .sin()
-        * CANVAS_SIZE.y
-        / 4.;
+    let image_mode = SpriteImageMode::Sliced(TextureSlicer {
+        border: BorderRect::axes(8., 19.),
+        center_scale_mode: SliceScaleMode::Stretch,
+        ..default()
+    });
+
+    let transform = Transform::from_xyz(CANVAS_SIZE.x / 2., 0.0, 1.0);
+    let gap_y_position = (time.elapsed_secs() * 4.2309875).sin() * CANVAS_SIZE.y / 4.;
     let pipe_offset = PIPE_SIZE.y / 2.0 + GAP_SIZE / 2.0;
 
-    commands.spawn((
-        transform,
-        Visibility::Visible,
-        Pipe,
-        children![
-            (
-                Sprite {
-                    image: image.clone(),
-                    custom_size: Some(PIPE_SIZE),
-                    image_mode: image_mode.clone(),
-                    ..default()
-                },
-                Transform::from_xyz(
-                    0.0,
-                    pipe_offset + gap_y_position,
-                    1.0,
-                ),
-                PipeTop
+    commands.spawn_scene(bsn! {
+        template_value(transform)
+        Visibility::Visible
+        Pipe
+        Children[
+            PipeTop
+            Sprite {
+                image: {image.clone()},
+                custom_size: PIPE_SIZE,
+                image_mode: {image_mode.clone()},
+            }
+            Transform::from_xyz(
+                0.0,
+                pipe_offset + gap_y_position,
+                1.0,
             ),
-            (
-                Visibility::Hidden,
-                Sprite {
-                    color: Color::WHITE,
-                    custom_size: Some(Vec2::new(
-                        10.0, GAP_SIZE,
-                    )),
-                    ..default()
-                },
-                Transform::from_xyz(
-                    0.0,
-                    gap_y_position,
-                    1.0,
+
+            PointsGate
+            Visibility::Hidden
+            Sprite {
+                color: Color::WHITE,
+                custom_size: Vec2::new(
+                    10.0, GAP_SIZE,
                 ),
-                PointsGate,
+            }
+            Transform::from_xyz(
+                0.0,
+                gap_y_position,
+                1.0,
             ),
-            (
-                Sprite {
-                    image,
-                    custom_size: Some(PIPE_SIZE),
-                    image_mode,
-                    ..default()
-                },
-                Transform::from_xyz(
-                    0.0,
-                    -pipe_offset + gap_y_position,
-                    1.0,
-                ),
-                PipeBottom,
-            )
-        ],
-    ));
+
+            PipeBottom
+            Sprite {
+                image,
+                custom_size: PIPE_SIZE,
+                image_mode,
+            }
+            Transform::from_xyz(
+                0.0,
+                -pipe_offset + gap_y_position,
+                1.0,
+            ),
+        ]
+    });
 }
 
-pub fn shift_pipes_to_the_left(
-    mut pipes: Query<&mut Transform, With<Pipe>>,
-    time: Res<Time>,
-) {
+pub fn shift_pipes_to_the_left(mut pipes: Query<&mut Transform, With<Pipe>>, time: Res<Time>) {
     for mut pipe in &mut pipes {
-        pipe.translation.x -=
-            PIPE_SPEED * time.delta_secs();
+        pipe.translation.x -= PIPE_SPEED * time.delta_secs();
     }
 }
 
-fn despawn_pipes(
-    mut commands: Commands,
-    pipes: Query<(Entity, &Transform), With<Pipe>>,
-) {
+fn despawn_pipes(mut commands: Commands, pipes: Query<(Entity, &Transform), With<Pipe>>) {
     for (entity, transform) in pipes.iter() {
-        if transform.translation.x
-            < -(CANVAS_SIZE.x / 2.0 + PIPE_SIZE.x)
-        {
+        if transform.translation.x < -(CANVAS_SIZE.x / 2.0 + PIPE_SIZE.x) {
             commands.entity(entity).despawn();
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 use bevy::{
     camera::ScalingMode,
     image::ImageAddressMode,
-    math::bounding::{
-        Aabb2d, BoundingCircle, IntersectsVolume,
-    },
+    math::bounding::{Aabb2d, BoundingCircle, IntersectsVolume},
     prelude::*,
     render::render_resource::AsBindGroup,
     shader::ShaderRef,
     sprite_render::{Material2d, Material2dPlugin},
 };
-use bevy::{
-    color::palettes::tailwind::RED_400,
-    image::ImageLoaderSettings,
-};
+use bevy::{color::palettes::tailwind::RED_400, image::ImageLoaderSettings};
 use flappy_bird::*;
 
 fn main() -> AppExit {
@@ -21,39 +16,29 @@ fn main() -> AppExit {
         .add_plugins(DefaultPlugins)
         .add_plugins((
             PipePlugin,
-            Material2dPlugin::<BackgroundMaterial>::default(
-            ),
+            Material2dPlugin::<BackgroundMaterial>::default(),
         ))
-        .add_systems(Startup, startup)
+        .add_systems(Startup, (startup_scene.spawn(), config_gizmos))
         .add_systems(
             FixedUpdate,
-            (
-                gravity,
-                check_in_bounds,
-                check_collisions,
-            )
-                .chain(),
+            (gravity, check_in_bounds, check_collisions).chain(),
         )
         .add_systems(
             Update,
             (
                 controls,
-                score_update
-                    .run_if(resource_changed::<Score>),
+                score_update.run_if(resource_changed::<Score>),
                 enforce_bird_direction,
             ),
         )
         .add_observer(respawn_on_endgame)
-        .add_observer(
-            |_trigger: On<ScorePoint>,
-             mut score: ResMut<Score>| {
-                score.0 += 1;
-            },
-        )
+        .add_observer(|_trigger: On<ScorePoint>, mut score: ResMut<Score>| {
+            score.0 += 1;
+        })
         .run()
 }
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 #[require(Gravity(1000.), Velocity)]
 struct Player;
 
@@ -72,94 +57,77 @@ struct Score(u32);
 #[derive(Event)]
 pub struct ScorePoint;
 
-#[derive(Component)]
+#[derive(Component, Default, Clone)]
 struct ScoreText;
 
-fn startup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut config_store: ResMut<GizmoConfigStore>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<BackgroundMaterial>>,
-) {
-    let (config, _) = config_store
-        .config_mut::<DefaultGizmoConfigGroup>();
-    config.enabled = false;
-
-    commands.spawn((
-        Camera2d,
-        Projection::Orthographic(OrthographicProjection {
+fn startup_scene() -> impl SceneList {
+    bsn_list! {
+        Camera2d
+        template_value(Projection::Orthographic(OrthographicProjection {
             scaling_mode: ScalingMode::AutoMax {
                 max_width: CANVAS_SIZE.x,
                 max_height: CANVAS_SIZE.y,
             },
             ..OrthographicProjection::default_2d()
-        }),
-    ));
+        })),
 
-    commands.spawn((
-        Player,
+        Player
         Sprite {
-            custom_size: Some(Vec2::splat(PLAYER_SIZE)),
-            image: asset_server.load("bevy-bird.png"),
-            color: Srgba::hex("#282828").unwrap().into(),
-            ..default()
+            custom_size: Vec2::splat(PLAYER_SIZE),
+            image: "bevy-bird.png",
+            color: {Color::from(Srgba::hex("#282828").unwrap())},
+        }
+        Transform {
+            translation: Vec3::new(-CANVAS_SIZE.x / 4.0, 0.0, 1.0)
         },
-        Transform::from_xyz(-CANVAS_SIZE.x / 4.0, 0.0, 1.0),
-    ));
 
-    commands.spawn((
         Node {
             width: percent(100.),
-            margin: px(20.).top(),
-            ..default()
-        },
-        Text::new("0"),
-        TextLayout::new_with_justify(Justify::Center),
-        TextFont {
-            font_size: 33.0,
-            ..default()
-        },
-        TextColor(Srgba::hex("#282828").unwrap().into()),
-        ScoreText,
-    ));
+            margin: {px(20.).top()},
+            justify_content: JustifyContent::Center
+        }
+        Children [
+            Text::new("0")
+            TextFont {
+                font_size: FontSize::Px(33.0),
+            }
+            TextColor({
+                Srgba::hex("#282828").unwrap()
+            })
+            ScoreText,
+        ],
 
-    commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(
-            CANVAS_SIZE.x,
-            CANVAS_SIZE.x,
-        ))),
-        MeshMaterial2d(materials.add(BackgroundMaterial {
-            color_texture: asset_server.load_with_settings(
-                "background_color_grass.png",
-                |settings: &mut ImageLoaderSettings| {
-                    settings
-                        .sampler
-                        .get_or_init_descriptor()
-                        .set_address_mode(
-                            ImageAddressMode::Repeat,
-                        );
-                },
-            ),
-        })),
-    ));
+        Mesh2d(asset_value(Rectangle::new(CANVAS_SIZE.x, CANVAS_SIZE.x)))
+        template(|context| {
+            let color_texture = context.resource::<AssetServer>()
+                    .load_builder()
+                    .with_settings(|settings: &mut ImageLoaderSettings| {
+                        settings
+                            .sampler
+                            .get_or_init_descriptor()
+                            .set_address_mode(ImageAddressMode::Repeat);
+                    })
+                    .load("background_color_grass.png");
+
+            let handle = context.resource_mut::<Assets<BackgroundMaterial>>().add(
+                BackgroundMaterial {
+                    color_texture
+                }
+            );
+            Ok(MeshMaterial2d(handle))
+        })
+    }
+}
+fn config_gizmos(mut config_store: ResMut<GizmoConfigStore>) {
+    let (config, _) = config_store.config_mut::<DefaultGizmoConfigGroup>();
+    config.enabled = false;
 }
 
-fn gravity(
-    mut transforms: Query<(
-        &mut Transform,
-        &mut Velocity,
-        &Gravity,
-    )>,
-    time: Res<Time>,
-) {
-    for (mut transform, mut velocity, gravity) in
-        &mut transforms
-    {
+fn gravity(mut transforms: Query<(&mut Transform, &mut Velocity, &Gravity)>, time: Res<Time>) {
+    for (mut transform, mut velocity, gravity) in &mut transforms {
         velocity.0 -= gravity.0 * time.delta_secs();
 
-        transform.translation.y +=
-            velocity.0 * time.delta_secs();
+        transform.translation.y += velocity.0 * time.delta_secs();
     }
 }
 
@@ -167,22 +135,14 @@ fn controls(
     mut velocity: Single<&mut Velocity, With<Player>>,
     buttons: Res<ButtonInput<MouseButton>>,
 ) {
-    if buttons.any_just_pressed([
-        MouseButton::Left,
-        MouseButton::Right,
-    ]) {
+    if buttons.any_just_pressed([MouseButton::Left, MouseButton::Right]) {
         velocity.0 = 400.;
     }
 }
 
-fn check_in_bounds(
-    player: Single<&Transform, With<Player>>,
-    mut commands: Commands,
-) {
-    if player.translation.y
-        < -CANVAS_SIZE.y / 2.0 - PLAYER_SIZE
-        || player.translation.y
-            > CANVAS_SIZE.y / 2.0 + PLAYER_SIZE
+fn check_in_bounds(player: Single<&Transform, With<Player>>, mut commands: Commands) {
+    if player.translation.y < -CANVAS_SIZE.y / 2.0 - PLAYER_SIZE
+        || player.translation.y > CANVAS_SIZE.y / 2.0 + PLAYER_SIZE
     {
         commands.trigger(EndGame);
     }
@@ -204,20 +164,14 @@ fn respawn_on_endgame(
 fn check_collisions(
     mut commands: Commands,
     player: Single<(&Sprite, Entity), With<Player>>,
-    pipe_segments: Query<
-        (&Sprite, Entity),
-        Or<(With<PipeTop>, With<PipeBottom>)>,
-    >,
+    pipe_segments: Query<(&Sprite, Entity), Or<(With<PipeTop>, With<PipeBottom>)>>,
     pipe_gaps: Query<(&Sprite, Entity), With<PointsGate>>,
     mut gizmos: Gizmos,
     transform_helper: TransformHelper,
 ) -> Result<()> {
-    let player_transform = transform_helper
-        .compute_global_transform(player.1)?;
-    let player_collider = BoundingCircle::new(
-        player_transform.translation().xy(),
-        PLAYER_SIZE / 2.,
-    );
+    let player_transform = transform_helper.compute_global_transform(player.1)?;
+    let player_collider =
+        BoundingCircle::new(player_transform.translation().xy(), PLAYER_SIZE / 2.);
 
     gizmos.circle_2d(
         player_transform.translation().xy(),
@@ -226,8 +180,7 @@ fn check_collisions(
     );
 
     for (sprite, entity) in &pipe_segments {
-        let pipe_transform = transform_helper
-            .compute_global_transform(entity)?;
+        let pipe_transform = transform_helper.compute_global_transform(entity)?;
         let pipe_collider = Aabb2d::new(
             pipe_transform.translation().xy(),
             sprite.custom_size.unwrap() / 2.,
@@ -244,8 +197,7 @@ fn check_collisions(
     }
 
     for (sprite, entity) in &pipe_gaps {
-        let gap_transform = transform_helper
-            .compute_global_transform(entity)?;
+        let gap_transform = transform_helper.compute_global_transform(entity)?;
         let gap_collider = Aabb2d::new(
             gap_transform.translation().xy(),
             sprite.custom_size.unwrap() / 2.,
@@ -266,16 +218,13 @@ fn check_collisions(
     Ok(())
 }
 
-fn score_update(
-    mut query: Query<&mut Text, With<ScoreText>>,
-    score: Res<Score>,
-) {
+fn score_update(mut query: Query<&mut Text, With<ScoreText>>, score: Res<Score>) {
     for mut span in &mut query {
         span.0 = score.0.to_string();
     }
 }
 
-#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone, FromTemplate)]
 pub struct BackgroundMaterial {
     #[texture(0)]
     #[sampler(1)]
@@ -288,15 +237,7 @@ impl Material2d for BackgroundMaterial {
     }
 }
 
-fn enforce_bird_direction(
-    mut player: Single<
-        (&mut Transform, &Velocity),
-        With<Player>,
-    >,
-) {
-    let calculated_velocity =
-        Vec2::new(PIPE_SPEED, player.1.0);
-    player.0.rotation = Quat::from_rotation_z(
-        calculated_velocity.to_angle(),
-    );
+fn enforce_bird_direction(mut player: Single<(&mut Transform, &Velocity), With<Player>>) {
+    let calculated_velocity = Vec2::new(PIPE_SPEED, player.1.0);
+    player.0.rotation = Quat::from_rotation_z(calculated_velocity.to_angle());
 }
